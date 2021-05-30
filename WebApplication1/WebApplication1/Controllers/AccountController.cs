68c2f6ea4d41abc8aec3fbd3a2e0cc6269d2ff0e
197PM09476 - Nguyễn Thành Luân - K25T-PM <luan.197pm09476@vanlanguni.vn>
2021-05-30 15:24:53 +0700

    Update Account

--- WebApplication1/WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/WebApplication1/Controllers/AccountController.cs
@@ -87,7 +87,7 @@ namespace WebApplication1.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Nhập sai mật khẩu");
                     return View(model);
             }
         }
