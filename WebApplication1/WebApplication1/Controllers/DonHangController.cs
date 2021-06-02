c6c241fcee03ede25cd76207c58f153dd7158b22
197PM09443 - Nguyễn Phúc Hậu - K25T-PM <hau.197pm09443@vanlanguni.vn>
2021-06-02 17:39:42 +0700

    DonHang1.1

---
+++ WebApplication1/WebApplication1/Controllers/DonHangController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DonHangController : Controller
+    {
+        private CT25Team15Entities db = new CT25Team15Entities();
+        private List<CHITIETGIOHANG> ShoppingCart = null;
+        public void ShoppingCartController()
+        {
+            var session = System.Web.HttpContext.Current.Session;
+            if (session["ShoppingCart"] != null)
+                ShoppingCart = session["ShoppingCart"] as List<CHITIETGIOHANG>;
+            else
+            {
+                ShoppingCart = new List<CHITIETGIOHANG>();
+                session["ShoppingCart"] = ShoppingCart;
+            }
+        }
+
+        // GET: DonHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            ShoppingCartController();
+            ViewBag.Cart = ShoppingCart;
+            return View();
+
+        }
+    }
+}
